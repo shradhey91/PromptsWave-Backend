1928cefcb8adb8235bc0ca6d666efa4103b0715e
shradhey91 <shradheyrastogi91@gmail.com>
2026-06-07 09:58:46 +0530

    fix Swagger server URL to use configured base URL for HTTPS

--- src/main/java/com/promptswave/config/SwaggerConfig.java
+++ src/main/java/com/promptswave/config/SwaggerConfig.java
@@ -5,11 +5,18 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 public class SwaggerConfig {
+
+    @Value("${app.base-url}")
+    private String baseUrl;
 
     @Bean
     public OpenAPI openAPI() {
@@ -18,6 +25,7 @@ public class SwaggerConfig {
                         .title("PromptsWave API")
                         .description("One-stop platform for expert AI prompts")
                         .version("1.0.0"))
+                .servers(List.of(new Server().url(baseUrl)))
                 .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
                 .components(new Components()
                         .addSecuritySchemes("Bearer Authentication",
