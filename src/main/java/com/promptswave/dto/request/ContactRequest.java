9197c2ee1f9991c5b7cb0d3076fbd598b73d616e
shradhey91 <shradheyrastogi91@gmail.com>
2026-06-13 15:50:41 +0530

    added contact us email

--- src/main/java/com/promptswave/dto/request/ContactRequest.java
+++ src/main/java/com/promptswave/dto/request/ContactRequest.java
@@ -6,13 +6,13 @@ import jakarta.validation.constraints.Size;
 
 public record ContactRequest(
 
-        @NotBlank(message = "First name is required") @Size(max = 100, message = "First name is too long") String firstName,
+                @NotBlank(message = "First name is required") @Size(max = 100, message = "First name is too long") String firstName,
 
-        @Size(max = 100, message = "Last name is too long") String lastName,
+                @Size(max = 100, message = "Last name is too long") String lastName,
 
-        @NotBlank(message = "Email is required") @Email(message = "Invalid email format") String email,
+                @NotBlank(message = "Email is required") @Email(message = "Invalid email format") String email,
 
-        @NotBlank(message = "Subject is required") @Size(max = 200, message = "Subject is too long") String subject,
+                @NotBlank(message = "Subject is required") @Size(max = 200, message = "Subject is too long") String subject,
 
-        @NotBlank(message = "Message is required") @Size(max = 5000, message = "Message is too long") String message) {
+                @NotBlank(message = "Message is required") @Size(max = 5000, message = "Message is too long") String message) {
 }
